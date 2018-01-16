f7e3267631ea749edcab551b8f85aba03c69a958
gksde <gksde@DESKTOP-OK5T8TP>
2018-01-16 21:24:45 +0530

    commit-2018/01/16-SERVER

---
+++ src/com/brotherssoft/samodconstructions/db/DBConnection.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.brotherssoft.samodconstructions.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author gksde
+ */
+public class DBConnection {
+    
+    private static DBConnection dbConnection;
+    private Connection connection;
+    
+    private DBConnection() throws ClassNotFoundException, SQLException{
+        Class.forName("com.mysql.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost/new_samod_constructions","root","suda");        
+    }
+    
+    public static DBConnection getDBConnection() throws ClassNotFoundException, SQLException{
+        if(dbConnection == null){
+            dbConnection = new DBConnection();
+        }
+        return dbConnection;
+    }
+    
+    public Connection getConnection(){
+        return connection;
+    }
+    
+}
